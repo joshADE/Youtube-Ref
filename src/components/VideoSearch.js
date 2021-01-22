8c6a8d31dd525dd64b98a1e76d4a968aee05b3d7
Josh A <joshmail9510@gmail.com>
2021-01-22 10:56:46 -0500

    Changed title, icons, added video validation

--- src/components/VideoSearch.js
+++ src/components/VideoSearch.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Button, Input, Label } from 'reactstrap';
+import ErrorNotice from './misc/ErrorNotice';
 
 function VideoSearch({
     setNewURL,
@@ -7,9 +8,17 @@ function VideoSearch({
 }) {
 
     const [url, setURL] = useState('');
+    const [error, setError] = useState();
 
     const handleSubmit = e => {
         e.preventDefault();
+        let pattern = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)?/i;
+        let result = pattern.test(url);
+        if (!result){
+            setError('The link provided wasn\'t a valid youtube link');
+            return;
+        }
+
         setNewURL(url);
         goToNextComponent();
 
@@ -19,8 +28,9 @@ function VideoSearch({
             className="d-flex flex-column justify-content-center align-items-center px-2"
         >
                 <Label for="videoSearch">Enter the url of the video to begin</Label>
-                <Input className="col-sm-5" type="text" name="videoSearch" id="videoSearch" value={url} onChange={e => setURL(e.target.value)} />
-                <Button onClick={handleSubmit}>Search</Button>
+                <Input className="col-sm-5 my-2" type="text" name="videoSearch" id="videoSearch" value={url} onChange={e => setURL(e.target.value)} />
+                {error && (<ErrorNotice message={error} clearError={() => setError(undefined)} />)}
+                <Button className="my-2" onClick={handleSubmit}>Search</Button>
         </div>
     )
 }
